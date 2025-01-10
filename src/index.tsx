7e953b52e56245d00a80784cd5acef1c566edf68
mykhalenych <mykhalenych@gmail.com>
2025-01-10 09:49:20 +0200

    bugfix/styles fixed import issue, renamed files

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 
 import './styles/index.scss';
 
-
 import { App } from './App';
 
 const container = document.getElementById('root') as HTMLDivElement;
